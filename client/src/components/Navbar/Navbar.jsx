ee400d9b7f8b44ad880bc5629376a3900010c4c0
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-08-20 22:23:23 -0700

    Working on home UI

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import ProfileCard from "../Cards/ProfileCard";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
   return (
     <header id="navbar">
-      <nav>
+      <nav className="flex justify-between items-center">
         <div className="nav-head">
           <Link to={"/"}>
             <h1 className="text-xl font-medium">NoteHive</h1>
           </Link>
+        </div>
+
+        <div className="nav-search">
+          <SearchBar />
+        </div>
+
+        <div className="nav-profile">
+          <ProfileCard />
         </div>
       </nav>
     </header>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1 className="text-center">Home</h1>
-    </div>
+      <div id="home">
+        <div id="home-background">
+          <h1 className="text-[12vw] text-slate-300">NoteHive.</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
