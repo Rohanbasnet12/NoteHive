3c5bddc6288a4723edb0b98faf49335eddbd3f1f
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-08-21 22:06:11 -0700

    Completed NoteCard UI

---
+++ client/src/components/Cards/NoteCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="z-20 border-2 p-4 rounded hover:shadow-xl transition-all ease relative">
+      <div className="flex items-center justify-between">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+
+        <i className="fa-solid fa-thumbtack" onClick={onPinNote}></i>
+      </div>
+
+      <p className="text-xm text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xs text-slate-500">{tags}</div>
+
+        <div className="flex items-center gap-4 text-slate-400">
+          <i className="fa-solid fa-pen" onClick={onEdit}></i>
+          <i className="fa-solid fa-trash" onClick={onDelete}></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import "./Home.css";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <div id="home">
-        <div id="home-background">
+        <div
+          id="home-background"
+          className="position absolute top-[50%] left-[50%] z-10 opacity-30"
+          style={{ transform: "translate(-50%, -50%)" }}
+        >
           <h1 className="text-[12vw] text-slate-300">NoteHive.</h1>
+        </div>
+        <div className="container mx-auto">
+          <div className="grid grid-cols-3 gap-4 mt-8">
+            <NoteCard
+              title="Meeting on 7th April"
+              date="3rd April 2024"
+              content="Meeting on 7th April date of information 3rd April 2024"
+              tags="#meetings"
+              isPinned={true}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+          </div>
         </div>
       </div>
     </>
