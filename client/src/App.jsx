c35e3317ec906129506bd308ce7efba8da31516a
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-10-15 23:21:00 -0700

    Setting up the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div id="app">
-      <h1>NoteHive</h1>
-    </div>
+    <Router>
+      <div id="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav id="navbar">
+        <div className="nav-head">
+          <h1>
+            NoteHive
+            <span className="text-orange-600">.</span>
+          </h1>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
