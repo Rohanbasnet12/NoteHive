d1d3cc6730e11298142c82ce7b4c6f6b8524805b
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-10-28 22:59:47 -0700

    Completed the working of the search bar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileCard from "./ProfileCard";
 import SearchBar from "./SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    setSearchQuery("");
+  };
+
   return (
     <header className="z-50 sticky top-0 left-0 shadow-xl py-4 px-6">
       <nav id="navbar" className="flex items-center justify-between">
@@ -13,7 +19,13 @@ const Navbar = () => {
           </h1>
         </div>
         <div className="nav-searchBar">
-          <SearchBar />
+          <SearchBar
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            handleSearch={handleSearch}
+          />
         </div>
         <div className="nav-profile-card">
           <ProfileCard />
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange, handleSearch }) => {
   return (
     <div id="searchBar">
-      <div className="searchBar_wrapper border-2 border-black rounded-md flex items-center justify-between px-2 gap-2">
-        <input type="text" className="outline-none w-full py-1" />
-        <button>
-          <i class="fa-solid fa-magnifying-glass bg-transparent"></i>
+      <div className="searchBar_wrapper border-2 border-gray-500 rounded-md flex items-center justify-between px-2 gap-2">
+        <input
+          type="text"
+          name="searchBar"
+          id="searchBar"
+          placeholder="Search Notes"
+          value={value}
+          onChange={onChange}
+          className="outline-none py-1 lg:w-[280px] md:w-[250px] smw-[200px]"
+        />
+
+        <button type="submit" onClick={handleSearch}>
+          <i className="fa-solid fa-magnifying-glass bg-transparent text-gray-600 hover:text-black"></i>
         </button>
       </div>
     </div>
