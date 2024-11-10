75962a53af04ea7a5aeecedeadb899bdae361818
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-10 11:44:13 +0530

    Creating logics for creatig user using backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 app.use(
@@ -8,8 +10,36 @@ app.use(
   })
 );
 
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
 app.get("/", (req, res) => {
   res.json({ data: "Hello" });
+});
+
+// Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
 });
 
 app.listen(3000);
