e2d1a2ae5befd45a95711283d46ab7c964d42cea
Rohanbasnet12 <rohanbmyname@gmail.com>
2025-01-02 22:22:30 +0530

    Erorr

--- server/server.js
+++ server/server.js
@@ -24,7 +24,11 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Required for self-signed certificates
+  },
 });
+
 db.connect();
 
 app.get("/", (req, res) => {
